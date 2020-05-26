2c459e00dcb7c0391e8ac57887d7da19c1e8eabf
Jonatan Machado <jonatanmachado35@gmail.com>
2020-05-26 16:35:14 -0300

    finalizando home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Filter,
+  Container,
+  ProductsQuant,
+  Products,
+  Product,
+  Details,
+  Buy,
+} from "./style";
+const randomicUser = Math.floor(Math.random() * 10);
+
+const products = [
+  {
+    name: "Item E",
+    price: 40.5,
+    img: `https://source.unsplash.com/collection/${randomicUser}/200x200`,
+  },
+  {
+    name: "Item B",
+    price: 55.9,
+    img: `https://i.picsum.photos/id/${randomicUser + 1}/200/200.jpg`,
+  },
+  {
+    name: "Item D",
+    price: 80.0,
+    img: `https://i.picsum.photos/id/${randomicUser + 2}/200/200.jpg`,
+  },
+  {
+    name: "Item C",
+    price: 99.0,
+    img: `https://i.picsum.photos/id/${randomicUser + 3}/200/200.jpg`,
+  },
+  {
+    name: "Item A",
+    price: 199.99,
+    img: `https://i.picsum.photos/id/${randomicUser + 4}/200/200.jpg`,
+  },
+  {
+    name: "Item H",
+    price: 210.0,
+    img: `https://i.picsum.photos/id/${randomicUser + 5}/200/200.jpg`,
+  },
+  {
+    name: "Item F",
+    price: 499.99,
+    img: `https://i.picsum.photos/id/${randomicUser + 6}/200/200.jpg`,
+  },
+  {
+    name: "Item G",
+    price: 501.0,
+    img: `https://i.picsum.photos/id/${randomicUser + 7}/200/200.jpg`,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ProductsQuant>
+        Quantidade de produtos: {products.length}
+        <select name="" id="">
+          <option value="cresc">Preço:Crescente</option>
+          <option value="desc">Preço:Decrescente</option>
+        </select>
+      </ProductsQuant>
+      <Filter>
+        <div>
+          <h2>Filtros:</h2>
+          <div>
+            <label>Valor Minimo:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Valor Maximo:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Buscar Produto:</label>
+            <input type="text" />
+          </div>
+        </div>
+      </Filter>
+
+      <Products>
+        {products.map((product) => (
+          <Product>
+            <img src={product.img} />
+            <Details>{product.name}</Details>
+            <Details>R$ {product.price}</Details>
+            <Buy onClick={() => {}}>Adicionar ao Carrinho</Buy>
+          </Product>
+        ))}
+      </Products>
+    </Container>
   );
 }
 
