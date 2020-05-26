308f0f75525eebe691297eae05550c7b0a2947ca
Alexandre Gessone <alex.gessone@gmail.com>
2020-05-26 16:51:45 -0300

    Segue a nova versão

--- src/Components/Filtro.js
+++ src/Components/Filtro.js
@@ -11,24 +11,10 @@ export class Filtro extends React.Component {
           <h3>Filtro</h3>
             
             <p>Valor Mínimo  
-                <select>
-                    <option>5,00</option>
-                    <option>10,00</option>
-                    <option>15,00</option>
-                    <option>20,00</option>
-                    <option>25,00</option>
-                </select> 
                 <input></input>
              </p>
              
              <p>Valor Máximo
-                <select>
-                    <option>5,00</option>
-                    <option>10,00</option>
-                    <option>15,00</option>
-                    <option>20,00</option>
-                    <option>25,00</option>
-                </select> 
                 <input></input>
              </p>
             <p>Buscar Produto
